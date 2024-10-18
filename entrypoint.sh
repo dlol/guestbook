809a5ece86ab7484dd9f8ef91e668624581ab2e6
dw <dw@konakona.moe>
2024-10-18 14:16:17 +0200

    fix(entrypoint): cp -r

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "$(ls -A "/guestbook/src/static")" ] && cp static-tmp/* /guestbook/src/static
+[ -z "$(ls -A "/guestbook/src/static")" ] && cp -r static-tmp/* /guestbook/src/static
 [ ! -f "/guestbook/config/config.yml" ]   && cp /guestbook/config.yml /guestbook/config/config.yml
 
 node src/index.js --docker
